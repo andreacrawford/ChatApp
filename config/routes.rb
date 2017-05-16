86d4618581bd4a16c102dbd17386112bf10baf3e
Andrea <andrea@crawford.cc>
2017-05-16 13:57:05 -0700

    first commit/devise

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'static_pages/index'
+  root 'static_pages#index'
+end
