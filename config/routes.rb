a506cb129b290f967032fb98c59697ab8f5425d5
Andrea <andrea@crawford.cc>
2017-05-16 21:54:13 -0700

    Beginning styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'static_pages/index'
-  root 'static_pages#index'
+  get 'rooms/index'
+  root 'rooms#index'
 end
